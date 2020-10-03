8cc0fae462546e1432f40da7c90719c03b0f240b
javyduty <javydutyalante@gmail.com>
2020-10-02 21:11:41 -0400

    here you go gilbert

--- AngularMinglr/src/app/registration/registration.component.ts
+++ AngularMinglr/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+  
+  test = ''
+  password = '';
+  username = '';
+  existingUsers: any;
+
+  onSubmit(){
+    console.log("in onSubmit");
+    this.userService.addUser();
+  }
+
+  onChange(value){
+    // console.log("in onChange");
+    this.test = value;
+    if(value == this.password){
+      console.log("passwords match")
+    }
+  }
+
+  getUsername(name){
+    this.username = name;
+    for(var i=0; i < this.existingUsers.length; i++){
+      if(this.username == this.existingUsers[i].userName){
+        console.log("user exists");
+      }
+    }
+  }
+
+  getPassword(value){
+    this.password = value;
+  }
 
   ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(data =>{
+      this.existingUsers = data;
+    });
   }
 
 }

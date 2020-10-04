fa4fc4c416f0a6a934500eb8bdb4daab14682b52
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-10-03 20:10:49 -0500

    User story complete & routing implemented

--- AngularMinglr/src/app/app-routing.module.ts
+++ AngularMinglr/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
@@ -8,6 +9,8 @@ const routes: Routes = [
   {path:'login', component: LoginComponent}
 
 ];
+
+export const routingComponents =[RegistrationComponent,LoginComponent];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularMinglr/src/app/app.module.ts
+++ AngularMinglr/src/app/app.module.ts
@@ -2,26 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { AjaxService } from './ajax.service';
-import { RegistrationComponent } from './registration/registration.component';
+
 import { NavigationComponent } from './navigation/navigation.component';
 import { GlobalfeedComponent } from './globalfeed/globalfeed.component';
 import { CommentComponent } from './comment/comment.component';
 import { SearchComponent } from './search/search.component';
 
-import {RouterModule} from "@angular/router";
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent,
+    routingComponents,
     NavigationComponent,
     GlobalfeedComponent,
     CommentComponent,
@@ -31,11 +27,9 @@ import {RouterModule} from "@angular/router";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule
+    FormsModule
   ],
   providers: [AjaxService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-

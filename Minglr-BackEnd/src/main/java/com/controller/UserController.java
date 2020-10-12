aa02d5de954db12779fe3b7e355039145e4f6962
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-10-11 21:39:01 -0500

    test

--- Minglr-BackEnd/src/main/java/com/controller/UserController.java
+++ Minglr-BackEnd/src/main/java/com/controller/UserController.java
@@ -16,7 +16,7 @@ import com.models.User;
 
 @Controller
 @RequestMapping(value = "/api")
-@CrossOrigin(origins = "http://d2833sfl28pbuq.cloudfront.net") //will be changed for a specific endpoint
+@CrossOrigin(origins = "http://minglrs3bucket.s3-website.us-east-2.amazonaws.com") //will be changed for a specific endpoint
 public class UserController {
 	
 	@Autowired
